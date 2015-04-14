bd15a9ab1dbdeac1c597948a9c02122dac6bdcea
RyanVrecenar <Ryanvre@yahoo.com>
2015-04-13 17:30:21 -0700

    Pushing Update
    Update

--- A4.py
+++ A4.py
@@ -2,7 +2,7 @@
 
 import os, sys, subprocess, signal, base64, array, binascii, random, re, math
 from subprocess import Popen, PIPE
-import pyxed, pydasm, pefile, inspect
+import pyxed, pydasm, pefile
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -91,7 +91,7 @@ def decode_main(filename):
 	if m is not None:
 		print(m.start())
 		# LOOK FOR WHERE CODE SECTION BEGINS
-		if(m.start()<2048):
+		if(m.start()<1024):
 			binhex_begin = int(m.start())/2
 			k = int(m.start())
 			out += "\n" +  "PE HEADER: " + reverse_hex(binhex[k: k+8])
@@ -133,6 +133,29 @@ def decode_main(filename):
 			image_base = reverse_hex(binhex[k:k+8])
 			out += "\n" +  "IMAGE BASE: " + image_base
 			k+=8
+	data_begin_addr = len(binhex)-1
+	code_begin_addr = 0
+	if(m is not None):
+		print int(reverse_hex(binhex[m.start()+48*2:m.start()+48*2+8]),16)
+		data_begin_addr =  int(reverse_hex(binhex[m.start()+48*2:m.start()+48*2+8]),16)*2
+		binhex_code = binhex[data_begin_addr:]
+		print  int(reverse_hex(binhex[m.start()+44*2:m.start()+44*2+8]),16)
+		code_begin_addr = int(reverse_hex(binhex[m.start()+44*2:m.start()+44*2+8]),16)
+	#binhex_code = binhex[code_begin_addr:data_begin_addr]
+	#binhex_code = binhex[code_begin_addr:code_begin_addr+0x3fa00]
+	#code_begin_addr = 2080
+	code_begin_addr = 4096/4
+	binhex_code = binhex[code_begin_addr*2:code_begin_addr*2+0xffffffff]
+	if m is None:
+		binhex_code = binhex
+	xed = pyxed.Decoder()
+	xed.set_mode(pyxed.XED_MACHINE_MODE_LEGACY_32, pyxed.XED_ADDRESS_WIDTH_32b)
+	hexdig = binhex_code
+	q = open('instructions.txt', 'w')
+	q.write(hexdig)
+	q.close()
+	xed.itext = binascii.unhexlify(hexdig)
+	xed.runtime_address = 0x00000000 + 4096
 
 	#/////////////////////////////////////////////////////////////////////Run Instructions Byte by Byte to create a Answer Key
 	q = 0 # tracks header pointer
@@ -143,6 +166,197 @@ def decode_main(filename):
 	#hexdig = binhex
 
 	instr_key = dict()
+	bad_c = 0
+	#loop through until terminate where p == q 
+	while True:
+		try:
+			xed.itext = binascii.unhexlify(hexdig[q:p]) # decodes bytes between header and tail
+			if(debug == 4):
+				out += "\n" +  (hexdig[q:p])
+			if(q == len(hexdig)):
+				break
+			xed.runtime_address = 0x00000000 + q/2 + int(image_base, 16) + code_begin_addr + 3072
+			inst = xed.decode()							# decodes bytes between header and tail	
+			instr_str = inst.dump_intel_format()
+			if(debug == 2):
+				out += "\n" +  " Address: " + instr_str[:8] + " Instruction: " + instr_str[9:]
+			instr_key[instr_str[:8]]	= instr_str[9:]
+			if(debug == 1):
+				out += "\n" +  (instr_str)							# dumps sucessful translation
+			xed = pyxed.Decoder()
+			xed.set_mode(pyxed.XED_MACHINE_MODE_LEGACY_32, pyxed.XED_ADDRESS_WIDTH_32b)
+			xed.runtime_address = 0x00000000 + q/2 + int(image_base, 16) + code_begin_addr + 3072
+			q = p 										# move head to tail
+			p = q + 2
+			continue
+		except:
+			if(debug == 3):
+				out += "\n" +  "Oops!  That was not a valid decode.  Try again..."
+				out += "\n" +  "p: "+ str(p) + " q: " + str(q) + " | "+ hexdig[q:p]
+				out += "\n" +  "LENGEHTH: " + str(len(hexdig[q:p]))
+			xed = pyxed.Decoder()
+			xed.set_mode(pyxed.XED_MACHINE_MODE_LEGACY_32, pyxed.XED_ADDRESS_WIDTH_32b)
+			if(p-q>28):
+				instr_key[str(q/2)]	= "BAD BYTE"
+				bad_c +=1
+				q+=2 
+				p = q
+			if(p <= len(hexdig)):
+				p += 2
+			else:
+				break
+			continue
+		
+	#/////////////////////////////////////////////////////////////////////Given this answer key run same binary and pick random start point
+	#/////////////////////////////////////////////////////////////////////Find number of bad instructions before realigned
+	# reset starting conditions
+	q = 0 # tracks header pointer
+	p = 2 # tracks tail pointer
+
+	#count number of bad bytes
+	instruction_offset = 0
+	#random starting point to determine realignment calculation
+	q = random.randint(0, len(hexdig)-1)
+	if q%2 == 1:
+		q+=1
+	initial_q = q
+	p = q+2 
+
+	if initial_q == len(hexdig):
+		instruction_offset = initial_q/2
+
+	#create a dictionary for comparison
+	instr_test = dict()
+	if(debug < 5):
+		out += "\n" +  "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+		out += "\n" +  "STARTING FILE AT " + str(q/2)
+
+	if q < len(hexdig):
+		while True:
+			try:
+				xed.itext = binascii.unhexlify(hexdig[q:p]) # decodes bytes between header and tail
+				if(debug== 4):
+					out += "\n" +  hexdig[q:p]
+				if(q == len(hexdig)):
+					break
+				xed.runtime_address = 0x00000000 + q/2 + int(image_base, 16) + code_begin_addr + 3072
+				inst = xed.decode()							# decodes bytes between header and tail	
+				instr_str = inst.dump_intel_format()
+				if(debug== 2):
+					out += "\n" +  " Address: " + instr_str[:8] + " Instruction: " + instr_str[9:]
+				instr_test[instr_str[:8]]	= instr_str[9:]
+				if(debug == 1):
+					out += "\n" +  instr_str							# dumps sucessful translation
+				xed = pyxed.Decoder()
+				xed.set_mode(pyxed.XED_MACHINE_MODE_LEGACY_32, pyxed.XED_ADDRESS_WIDTH_32b)
+				xed.runtime_address = 0x00000000 + q/2 + int(image_base, 16) + code_begin_addr
+				q = p 										# move head to tail
+				p = q + 2
+				continue
+			except:
+				if(debug == 3):
+					out += "\n" +  "Oops!  That was not a valid decode.  Try again..."
+					out += "\n" +  "p: "+ str(p) + " q: " + str(q) + " | "+hexdig[q:p]
+					out += "\n" +  "LENGEHTH: " + str(len(hexdig[q:p]))
+				xed = pyxed.Decoder()
+				xed.set_mode(pyxed.XED_MACHINE_MODE_LEGACY_32, pyxed.XED_ADDRESS_WIDTH_32b)
+				if(p-q>28):
+					instr_key[str(format(q/2, 'x').zfill(8)) ]	= "BAD BYTE"
+					q+=2
+					p = q
+				if(p <= len(hexdig)):
+					p += 2
+				else:
+					break
+				continue
+
+	if(debug <= 5):
+		# A KEY OF CORRECT PARSING TO COMPARE TO
+		out += "\n" +  "CONTENTS OF KEY DICTONARY"
+		f = open('Assembly.txt','w')
+		for key in sorted(instr_key):
+			f.write("%s: %s\n" % (key,instr_key[key]))
+		f.close()
+
+	found_align = 0
+	if(debug < 5):
+		out += "\n" +  "CONTENTS OF TEST DICTONARY"
+
+	for key in sorted(instr_test):
+		if instr_key.has_key(key) and not(found_align):
+			found_align = 1
+			instruction_offset = int(key,16) - int(image_base,16) - int(code_base,16) 
+		if(debug < 5):
+			out += "\n" +  "%s: %s" % (key,instr_test[key])
+
+	out += "\n" +  "NUMBER OF BYTES TO ALIGNMENT: " + str(instruction_offset-initial_q/2) + " BYTES. "
+	out += "\n" +  "NUMBER OF INVALID BYTES: " + str(bad_c) +"."
+
+	num_bytes_align_sum = 0
+	num_bytes_align_arr = []
+
+	for i in range(0,10):
+		q = 0 # tracks header pointer
+		p = 2 # tracks tail pointer
+
+		#count number of bad bytes
+		instruction_offset = 0
+		#random starting point to determine realignment calculation
+		q = random.randint(0, len(hexdig)-1)
+		if q%2 == 1:
+			q+=1
+		initial_q = q
+		p = q+2 
+
+		if initial_q == len(hexdig):
+			instruction_offset = initial_q/2
+
+		#create a dictionary for comparison
+		instr_test = dict()
+
+		if q < len(hexdig):
+			while True:
+				try:
+					xed.itext = binascii.unhexlify(hexdig[q:p]) # decodes bytes between header and tail
+					if(q == len(hexdig)):
+						break
+					xed.runtime_address = 0x00000000 + q/2 + int(image_base, 16) + code_begin_addr
+					inst = xed.decode()							# decodes bytes between header and tail	
+					instr_str = inst.dump_intel_format()
+					instr_test[instr_str[:8]]	= instr_str[9:]
+					xed = pyxed.Decoder()
+					xed.set_mode(pyxed.XED_MACHINE_MODE_LEGACY_32, pyxed.XED_ADDRESS_WIDTH_32b)
+					xed.runtime_address = 0x00000000 + q/2 + int(image_base, 16) + code_begin_addr
+					q = p 										# move head to tail
+					p = q + 2
+					continue
+				except:
+					xed = pyxed.Decoder()
+					xed.set_mode(pyxed.XED_MACHINE_MODE_LEGACY_32, pyxed.XED_ADDRESS_WIDTH_32b)
+					if(p-q>28):
+						instr_key[str(format(q/2, 'x').zfill(8)) ]	= "BAD BYTE"
+						q+=2
+						p = q
+					if(p <= len(hexdig)):
+						p += 2
+					else:
+						break
+					continue
+
+		found_align = 0
+
+		for key in sorted(instr_test):
+			if instr_key.has_key(key) and not(found_align):
+				found_align = 1
+				instruction_offset = int(key,16) - int(image_base,16) - int(code_base,16) 
+
+		num_bytes_align_sum += instruction_offset-initial_q/2
+		num_bytes_align_arr.append(instruction_offset-initial_q/2)
+
+	out += "\n" + "Trial results for number of bytes to align: \n"
+	for i in range(0, len(num_bytes_align_arr)):
+		out += str(num_bytes_align_arr[i]) + ", "
+	out += "\n" + "Average num bytes to align: " + str(num_bytes_align_sum / 10)
 				
 	if m is not None:
 		#////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -160,57 +374,12 @@ def decode_main(filename):
 			i = pydasm.get_instruction(data[offset:], pydasm.MODE_32)
 			if (pydasm.get_instruction_string(i, pydasm.FORMAT_INTEL, ep_ava+offset) is not None): 
 				p.write(str(format((int(starting_code,16))+offset,'x' ).zfill(8))+": "+pydasm.get_instruction_string(i, pydasm.FORMAT_INTEL, ep_ava+offset)+"\n")
-				instr_key[str(format((int(starting_code,16))+offset,'x' ).zfill(8))] = pydasm.get_instruction_string(i, pydasm.FORMAT_INTEL, ep_ava+offset)
 				#out += "\n" +  "one"
 				offset += i.length
 			else:
 				offset += 1
 		p.close()
 		
-		total = 0
-		#////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		# LIST OF IMPORTED FILE AND FUNCTIONS
-		p = open("Imported.txt", 'w')
-		for entry in pe.DIRECTORY_ENTRY_IMPORT:
-			p.write( "DLL: "+entry.dll+"\n")
-			for imp in entry.imports:
-				#total += imp.hint
-				#print hex(((imp.address-int(image_base,16)))*2)
-				p.write( '\tAddr: ' + hex(imp.address) +  ", Name:"+ imp.name +" hint: " + str(imp.hint)  + "\n")
-				#data = binhex[(imp.address-int(image_base,16))*2:(imp.address-int(image_base,16) + imp.hint)*2]
-				#p.write(data)
-				#print hex(imp.address-int(image_base,16)) +" : "+ str(len(data))+ " : " + hex(imp.address+imp.hint-int(image_base,16)) 
-				#offset = 0
-				#while offset < len(data):
-				#	i = pydasm.get_instruction(data[offset:], pydasm.MODE_32)
-				#	if (pydasm.get_instruction_string(i, pydasm.FORMAT_INTEL, ep_ava+offset) is not None): 
-				#		p.write("\t\t"+str(format((int(starting_code,16))+offset,'x' ).zfill(8))+": "+pydasm.get_instruction_string(i, pydasm.FORMAT_INTEL, ep_ava+offset)+"\n")
-				#		#out += "\n" +  "one"
-				#		offset += i.length
-				#	else:
-				#		offset += 1
-		#print "Total: " + str(total) + hex(len(binhex))
-		p.close()
-		#////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		
-		pe =  pefile.PE(filename)
-		ep = pe.OPTIONAL_HEADER.AddressOfEntryPoint
-		starting_code = str(int(code_base) + int(image_base))
-		ep_ava = ep+pe.OPTIONAL_HEADER.ImageBase
-		data = pe.get_memory_mapped_image()[ep-(ep_ava - (int(starting_code,16))):ep+0xffffffff]
-		offset = 0
-		p = open('Assembly.txt','w')
-		while offset < len(data):
-			i = pydasm.get_instruction(data[offset:], pydasm.MODE_32)
-			if (pydasm.get_instruction_string(i, pydasm.FORMAT_INTEL, ep_ava+offset) is not None): 
-				p.write(str(format((int(starting_code,16))+offset,'x' ).zfill(8))+": "+pydasm.get_instruction_string(i, pydasm.FORMAT_INTEL, ep_ava+offset)+"\n")
-				#out += "\n" +  "one"
-				offset += i.length
-			else:
-				offset += 1
-		p.close()
-		
-		'''		random start info
 		pe =  pefile.PE(filename)
 		ep = pe.OPTIONAL_HEADER.AddressOfEntryPoint
 		starting_code = str(int(code_base) + int(image_base))
@@ -232,26 +401,8 @@ def decode_main(filename):
 			else:
 				offset += 1
 		p.close()
-		'''
 		#////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	p = open("CFG.txt",'w')
-	for x in sorted(instr_key):
-		p.write(x +":"+ instr_key[x]+"\n")
-	p.close()
-	 
-	# print sections and sizes
-	for section in pe.sections:
-		 print (section.Name, hex(section.VirtualAddress),hex(section.Misc_VirtualSize), section.SizeOfRawData )
-	#/////////////////////////////////////////////////////////////////////////////////////////////////////
-	
-	# full dump info
-	p = open("Full.txt", 'w')		
-	p.write( pe.dump_info())	
-	p.close()
-	#/////////////////////////////////////////////////////////////////////////////////////////////////////
-	
-	
-	
+
 	return out
 
 def autolabel(ax, rects):
